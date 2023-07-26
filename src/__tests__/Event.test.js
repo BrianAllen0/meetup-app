7f1172092777e64a1c369e197b13d4e240b36f0b
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-25 17:25:08 -0700

    Added event tests

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,5 +1,48 @@
 import { render } from "@testing-library/react";
 
 describe("<Event /> component", () => {
-    test("events render properly", () => {});
+    beforeEach(() => {
+        showDetailsButton = EventComponent.queryByText("Show Details");
+    });
+
+    test("renders event title", () => {
+        expect(
+            EventComponent.queryByText(allEvents[0].summary)
+        ).toBeInTheDocument();
+    });
+
+    test("renders event start time", () => {
+        expect(
+            EventComponent.queryByText(allEvents[0].created)
+        ).toBeInTheDocument();
+    });
+
+    test("renders event location", () => {
+        expect(
+            EventComponent.queryByTestId(allEvents[0].location)
+        ).toBeInTheDocument();
+    });
+
+    test("renders event details button with the title (show details)", () => {
+        expect(showDetailsButton).toBeInTheDocument();
+    });
+
+    test("event details are hidden by default", () => {
+        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+    });
+
+    test("show details when user clicks 'show details' button", async () => {
+        const user = userEvent.setup();
+        await user.click(showDetailsButton);
+        expect(EventComponent.queryByText("Details")).toBeInTheDocument();
+    });
+
+    test("hide details when user clicks 'hide details' button", async () => {
+        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+        const user = userEvent.setup();
+        await user.click(showDetailsButton);
+        expect(EventComponent.queryByText("Details")).toBeInTheDocument();
+        await user.click(showDetailsButton);
+        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+    });
 });
