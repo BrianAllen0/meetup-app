ef53c26c1893dafaa9230764bc1e36583a5ab195
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-25 17:57:36 -0700

    Created NumberOfEvents tests

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+
+describe("<NumberOfEvents /> component", () => {
+    beforeEach(() => {
+        numberInput = NumberOfEventsComponent.queryByRole("textbox");
+    });
+    test("contains textbox", () => {
+        expect(numberInput).toBeInTheDocument();
+    });
+    test("default input value is 32", () => {
+        expect(numberInput.value).toBe("32");
+    });
+
+    test("input box responds to typing", async () => {
+        const user = userEvent.setup();
+        let initialValue = numberInput.value;
+        await user.type(numberInput, "{backspace}");
+        let newValue = numberInput.value;
+        expect(newValue).not.toBeGreaterThan(initialValue);
+    });
+});
