5cb5a729e8db1d54a2b359b5a3972977e4725e1f
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-27 16:40:30 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ const checkToken = async (accessToken) => {
     const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
     const result = await response.json();
     return result;
-};
+}; ///////////////////
 
 const removeQuery = () => {
     let newurl;
