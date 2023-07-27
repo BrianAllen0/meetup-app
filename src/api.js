b3c6d2fea6543c809b987505183432cecca7980c
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-27 16:20:40 -0700

    Revert "Update api.js"
    This reverts commit 32a618205b44bc7c1805a9e0b36662956b5b3b90.

--- src/api.js
+++ src/api.js
@@ -1,35 +1,46 @@
-// src/api.js
+import mockData from "./mockdata";
 
-import mockData from "./mock-data";
-
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
 export const extractLocations = (events) => {
     const extractedLocations = events.map((event) => event.location);
     const locations = [...new Set(extractedLocations)];
     return locations;
 };
 
-/**
- *
- * This function will fetch the list of all events
- */
+const checkToken = async (accessToken) => {
+    const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
+    const result = await response.json();
+    return result;
+};
+
+const removeQuery = () => {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+        newurl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        window.history.pushState("", "", newurl);
+    } else {
+        newurl = window.location.protocol + "//" + window.location.host;
+        window.history.pushState("", "", newurl);
+    }
+};
+
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(`https://gf33sipgtj.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`);
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+
+    return access_token;
+};
+
 export const getEvents = async () => {
     if (window.location.href.startsWith("http://localhost")) {
         return mockData;
     }
 
     const token = await getAccessToken();
-
     if (token) {
         removeQuery();
-        const url = "https://gf33sipgtj.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
+        const url = `https://gf33sipgtj.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
@@ -55,52 +66,4 @@ export const getAccessToken = async () => {
         return code && getToken(code);
     }
     return accessToken;
-};
-
-const checkToken = async (accessToken) => {
-    const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-    const result = await response.json();
-    return result;
-};
-
-const removeQuery = () => {
-    let newurl;
-    if (window.history.pushState && window.location.pathname) {
-        newurl = window.location.protocol + "//" + window.location.host + window.location.pathname;
-        window.history.pushState("", "", newurl);
-    } else {
-        newurl = window.location.protocol + "//" + window.location.host;
-        window.history.pushState("", "", newurl);
-    }
-};
-
-// getToken without try...catch
-
-// const getToken = async (code) => {
-//   const encodeCode = encodeURIComponent(code);
-//   const response = await fetch(
-//     'YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode
-//   );
-//   const { access_token } = await response.json();
-//   access_token && localStorage.setItem("access_token", access_token);
-
-//   return access_token;
-// };
-
-// getToken with try...catch
-
-const getToken = async (code) => {
-    try {
-        const encodeCode = encodeURIComponent(code);
-
-        const response = await fetch("https://gf33sipgtj.execute-api.us-east-1.amazonaws.com/dev/api/token" + "/" + encodeCode);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const { access_token } = await response.json();
-        access_token && localStorage.setItem("access_token", access_token);
-        return access_token;
-    } catch (error) {
-        error.json();
-    }
 };
