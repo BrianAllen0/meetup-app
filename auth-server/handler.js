72b75622c863eed59b7ec422d0bb1838cfe48d0a
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-27 16:33:21 -0700

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,37 +2,91 @@
 
 const { google } = require("googleapis");
 const calendar = google.calendar("v3");
-const SCOPES = [
-    "https://www.googleapis.com/auth/calendar.events.public.readonly",
-];
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ["https://brianallen0.github.io/meetup-app/"];
 
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    redirect_uris[0]
-);
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, redirect_uris[0]);
+
+// module.exports.getAuthURL = async () => {
+//     const authUrl = oAuth2Client.generateAuthUrl({
+//         access_type: "offline",
+//         scope: SCOPES,
+//     });
+//     return {
+//         statusCode: 200,
+//         headers: {
+//             "Access-Control-Allow-Origin": "*",
+//             "Access-Control-Allow-Credentials": true,
+//         },
+//         body: JSON.stringify({ authUrl }),
+//     };
+// };
 
 module.exports.getAuthURL = async () => {
+    /**
+     * Scopes array passed to the `scope` option.
+     * Any scopes passed must be enabled
+     * in the 'OAuth consent screen' settings in your project
+     * on your Google Console. Also, any passed scopes are the ones
+     * users will see when the consent screen is displayed to them.
+     */
+
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: "offline",
         scope: SCOPES,
     });
+
     return {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
         },
-        body: JSON.stringify({ authUrl }),
+        body: JSON.stringify({
+            authUrl,
+        }),
     };
 };
+
+// module.exports.getAccessToken = async (event) => {
+//     const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+//     return new Promise((resolve, reject) => {
+//         oAuth2Client.getToken(code, (error, response) => {
+//             if (error) {
+//                 return reject(error);
+//             }
+//             return resolve(response);
+//         });
+//     })
+//         .then((results) => {
+//             return {
+//                 statusCode: 200,
+//                 headers: {
+//                     "Access-Control-Allow-Origin": "*",
+//                     "Access-Control-Allow-Credentials": true,
+//                 },
+//                 body: JSON.stringify(results),
+//             };
+//         })
+//         .catch((error) => {
+//             return {
+//                 statusCode: 500,
+//                 body: JSON.stringify(error),
+//             };
+//         });
+// };
 
 module.exports.getAccessToken = async (event) => {
     const code = decodeURIComponent(`${event.pathParameters.code}`);
 
     return new Promise((resolve, reject) => {
+        /**
+         * Exchange authorization code for access token with a 'callback' after the exchange,
+         * The callback in this case is an arrow function with the results as paramenters: 'err' and 'token.'
+         */
+
         oAuth2Client.getToken(code, (error, response) => {
             if (error) {
                 return reject(error);
@@ -41,6 +95,7 @@ module.exports.getAccessToken = async (event) => {
         });
     })
         .then((results) => {
+            //Respond with OAtuth token
             return {
                 statusCode: 200,
                 headers: {
@@ -51,6 +106,7 @@ module.exports.getAccessToken = async (event) => {
             };
         })
         .catch((error) => {
+            console.log(error);
             return {
                 statusCode: 500,
                 body: JSON.stringify(error),
@@ -58,13 +114,51 @@ module.exports.getAccessToken = async (event) => {
         });
 };
 
-module.exports.getCalendarEvents = async (event) => {
-    return new Promise((resolve, reject) => {
-        const access_token = decodeURIComponent(
-            `${event.pathParameters.token}`
-        );
-        oAuth2Client.setCredentials({ access_token });
+// module.exports.getCalendarEvents = async (event) => {
+//     return new Promise((resolve, reject) => {
+//         const access_token = decodeURIComponent(`${event.pathParameters.token}`);
+//         oAuth2Client.setCredentials({ access_token });
 
+//         calendar.events.list(
+//             {
+//                 calendarId: CALENDAR_ID,
+//                 auth: oAuth2Client,
+//                 timeMin: new Date().toISOString(),
+//                 singleEvents: true,
+//                 orderBy: "startTime",
+//             },
+//             (error, response) => {
+//                 if (error) {
+//                     reject(error);
+//                 } else {
+//                     resolve(response);
+//                 }
+//             }
+//         );
+//     })
+//         .then((results) => {
+//             return {
+//                 statusCode: 200,
+//                 headers: {
+//                     "Access-Control-Allow-Origin": "*",
+//                     "Access-Control-Allow-Credentials": true,
+//                 },
+//                 body: JSON.stringify(results.data.items),
+//             };
+//         })
+//         .catch((error) => {
+//             return {
+//                 statusCode: 500,
+//                 body: JSON.stringify(error),
+//             };
+//         });
+// };
+
+module.exports.getCalendarEvents = async (event) => {
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject) => {
         calendar.events.list(
             {
                 calendarId: CALENDAR_ID,
@@ -89,7 +183,7 @@ module.exports.getCalendarEvents = async (event) => {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Credentials": true,
                 },
-                body: JSON.stringify(results.data.items),
+                body: JSON.stringify({ events: results.data.items }),
             };
         })
         .catch((error) => {
