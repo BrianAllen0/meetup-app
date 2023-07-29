51bac3666fbe77855bbe6049a02010c2810d8576
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-07-29 16:07:10 -0700

    Test overhaul

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,31 +1,32 @@
-import { render } from "@testing-library/react";
-import CitySearch from "../components/CitySearch";
+import { render, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import CitySearch from "../components/CitySearch";
+import App from "../App";
 import { extractLocations, getEvents } from "../api";
 
 describe("<CitySearch /> component", () => {
     let CitySearchComponent;
-    let cityTextBox;
-    let suggestionList;
-
     beforeEach(() => {
-        CitySearchComponent = render(<CitySearch />);
-        cityTextBox = CitySearchComponent.queryByRole("textbox");
-        suggestionList = CitySearchComponent.queryByRole("list");
+        CitySearchComponent = render(<CitySearch allLocations={[]} />);
     });
 
     test("renders text input", () => {
+        const cityTextBox = CitySearchComponent.queryByRole("textbox");
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass("city");
     });
 
     test("suggestions list is hidden by default", () => {
+        const suggestionList = CitySearchComponent.queryByRole("list");
         expect(suggestionList).not.toBeInTheDocument();
     });
 
-    test("renders a list of suggestions when city textbox gains focus", async () => {
+    test("renders a list of suggestions when city text box gains focus", async () => {
         const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole("textbox");
         await user.click(cityTextBox);
+
+        const suggestionList = CitySearchComponent.queryByRole("list");
         expect(suggestionList).toBeInTheDocument();
         expect(suggestionList).toHaveClass("suggestions");
     });
@@ -34,25 +35,18 @@ describe("<CitySearch /> component", () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(
-            <CitySearch allLocations={allLocations} />
-        );
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
         const cityTextBox = CitySearchComponent.queryByRole("textbox");
         await user.type(cityTextBox, "Berlin");
 
         const suggestions = allLocations
             ? allLocations.filter((location) => {
-                  return (
-                      location
-                          .toUpperCase()
-                          .indexOf(cityTextBox.value.toUpperCase()) > -1
-                  );
+                  return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
               })
             : [];
 
-        const suggestionListItems =
-            CitySearchComponent.queryAllByRole("listitem");
+        const suggestionListItems = CitySearchComponent.queryAllByRole("listitem");
         expect(suggestionListItems).toHaveLength(suggestions.length + 1);
         for (let i = 0; i < suggestions.length; i += 1) {
             expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
@@ -63,18 +57,32 @@ describe("<CitySearch /> component", () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(
-            <CitySearch allLocations={allLocations} />
-        );
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setCurrentCity={() => {}} />);
 
         const cityTextBox = CitySearchComponent.queryByRole("textbox");
         await user.type(cityTextBox, "Berlin");
 
-        const BerlinGermanySuggestion =
-            CitySearchComponent.queryAllByRole("listitem")[0];
-
+        const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole("listitem")[0];
         await user.click(BerlinGermanySuggestion);
 
         expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
+});
+
+describe("<CitySearch /> integration", () => {
+    test("renders suggestions list when the app is rendered.", async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+
+        const CitySearchDOM = AppDOM.querySelector("#city-search");
+        const cityTextBox = within(CitySearchDOM).queryByRole("textbox");
+        await user.click(cityTextBox);
+
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+
+        const suggestionListItems = within(CitySearchDOM).queryAllByRole("listitem");
+        expect(suggestionListItems.length).toBe(allLocations.length + 1);
     });
 });
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,49 +1,52 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { getEvents } from "../api";
+import Event from "../components/Event";
 
 describe("<Event /> component", () => {
-    beforeEach(() => {
-        showDetailsButton = EventComponent.queryByText("Show Details");
-        hideDetailsButton = EventComponent.queryByText("Hide Details");
+    let EventComponent;
+    let allEvents;
+    beforeEach(async () => {
+        allEvents = await getEvents();
+        EventComponent = render(<Event event={allEvents[0]} />);
     });
 
-    test("renders event title", () => {
-        expect(
-            EventComponent.queryByText(allEvents[0].summary)
-        ).toBeInTheDocument();
-    });
-
-    test("renders event start time", () => {
-        expect(
-            EventComponent.queryByText(allEvents[0].created)
-        ).toBeInTheDocument();
+    test("renders event Title", () => {
+        expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
     });
 
     test("renders event location", () => {
-        expect(
-            EventComponent.queryByTestId(allEvents[0].location)
-        ).toBeInTheDocument();
+        expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
     });
 
     test("renders event details button with the title (show details)", () => {
-        expect(showDetailsButton).toBeInTheDocument();
+        expect(EventComponent.queryByText("show details")).toBeInTheDocument();
     });
 
-    test("event details are hidden by default", () => {
-        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+    test("by default, event's details section should be hidden", () => {
+        expect(EventComponent.container.querySelector(".details")).not.toBeInTheDocument();
     });
 
-    test("show details when user clicks 'show details' button", async () => {
+    test("shows the details section when the user clicks on the 'show details' button", async () => {
         const user = userEvent.setup();
-        await user.click(showDetailsButton);
-        expect(EventComponent.queryByText("Details")).toBeInTheDocument();
+        await user.click(EventComponent.queryByText("show details"));
+
+        expect(EventComponent.container.querySelector(".details")).toBeInTheDocument();
+        expect(EventComponent.queryByText("hide details")).toBeInTheDocument();
+        expect(EventComponent.queryByText("show details")).not.toBeInTheDocument();
     });
 
-    test("hide details when user clicks 'hide details' button", async () => {
-        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+    test("hides the details section when the user clicks on the 'hide details' button", async () => {
         const user = userEvent.setup();
-        await user.click(hideDetailsButton);
-        expect(EventComponent.queryByText("Details")).toBeInTheDocument();
-        await user.click(hideDetailsButton);
-        expect(EventComponent.queryByText("Details")).not.toBeInTheDocument();
+
+        await user.click(EventComponent.queryByText("show details"));
+        expect(EventComponent.container.querySelector(".details")).toBeInTheDocument();
+        expect(EventComponent.queryByText("hide details")).toBeInTheDocument();
+        expect(EventComponent.queryByText("show details")).not.toBeInTheDocument();
+
+        await user.click(EventComponent.queryByText("hide details"));
+        expect(EventComponent.container.querySelector(".details")).not.toBeInTheDocument();
+        expect(EventComponent.queryByText("hide details")).not.toBeInTheDocument();
+        expect(EventComponent.queryByText("show details")).toBeInTheDocument();
     });
 });
