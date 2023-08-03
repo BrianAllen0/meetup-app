e7b3cbba3149314ac898d38116b49e1ea58a4ce6
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-08-02 17:08:18 -0700

    Added alert class and info alert subclass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import EventList from "./components/EventList";
 import CitySearch from "./components/CitySearch";
 import NumberOfEvents from "./components/NumberOfEvents";
+import infoAlert from "./components/Alert";
 import { extractLocations, getEvents } from "./api";
 
 import "./App.css";
@@ -11,6 +12,7 @@ const App = () => {
     const [currentNOE, setCurrentNOE] = useState(32);
     const [allLocations, setAllLocations] = useState([]);
     const [currentCity, setCurrentCity] = useState("See all cities");
+    const [infoAlert, setInfoAlert] = useState("");
 
     useEffect(() => {
         fetchData();
@@ -18,20 +20,15 @@ const App = () => {
 
     const fetchData = async () => {
         const allEvents = await getEvents();
-        const filteredEvents =
-            currentCity === "See all cities"
-                ? allEvents
-                : allEvents.filter((event) => event.location === currentCity);
+        const filteredEvents = currentCity === "See all cities" ? allEvents : allEvents.filter((event) => event.location === currentCity);
         setEvents(filteredEvents.slice(0, currentNOE));
         setAllLocations(extractLocations(allEvents));
     };
 
     return (
         <div className="App">
-            <CitySearch
-                allLocations={allLocations}
-                setCurrentCity={setCurrentCity}
-            />
+            <div className="alerts-container">{infoAlert.length ? <InfoAlert text={infoAlert} /> : null}</div>
+            <CitySearch setInfoAlert={setInfoAlert} allLocations={allLocations} setCurrentCity={setCurrentCity} />
             <NumberOfEvents />
             <EventList events={events} />
         </div>
